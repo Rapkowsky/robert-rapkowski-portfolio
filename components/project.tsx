dffb1e467f763434373554597ec67a910e7e9ec6
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-02-05 23:16:38 +0100

    feat: border-t for index 0

--- components/project.tsx
+++ components/project.tsx
@@ -21,7 +21,9 @@ export default function Project({ index, title, setModal }: ProjectProps) {
       onMouseLeave={() => {
         setModal({ active: false, index });
       }}
-      className="group flex w-full cursor-pointer items-center justify-between border-b-[0.1px] border-[#d7d7d8] px-[100px] py-[75px] text-[#b4b5b6] transition duration-300 hover:border-white"
+      className={`group flex w-full cursor-pointer items-center justify-between ${
+        index === 0 ? "border-t-[0.1px]" : ""
+      } border-b-[0.1px] border-[#d7d7d8] px-[100px] py-[75px] text-[#b4b5b6] transition duration-300 hover:border-white`}
     >
       <h2 className="m-0 text-[90px] font-[400] transition-all duration-300 ease-rrSmooth group-hover:translate-x-[-30px] group-hover:text-[white]">
         {title}
