7e8302af732068652198344c28bb34ccbb5e3cd1
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-02-05 02:32:13 +0100

    feat: add magnetic behavior

--- components/LinkComponent.tsx
+++ components/LinkComponent.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { motion } from "framer-motion";
 import { slide, scale } from "@/lib/Animations";
+import MagneticWrapper from "./MagneticWrapper";
 
 type LinkProps = {
   data: {
@@ -20,25 +21,27 @@ export default function LinkComponent({
   const { title, href, index } = data;
 
   return (
-    <motion.div
-      className="relative flex items-center"
-      onMouseEnter={() => {
-        setSelectedIndicator(href);
-      }}
-      custom={index}
-      variants={slide}
-      initial="initial"
-      animate="enter"
-      exit="exit"
-    >
+    <MagneticWrapper>
       <motion.div
-        variants={scale}
-        animate={isActive ? "open" : "closed"}
-        className="absolute left-[-30px] h-[10px] w-[10px] rounded-full bg-white"
-      ></motion.div>
-      <Link href={href} className="font-light text-white no-underline">
-        {title}
-      </Link>
-    </motion.div>
+        className="relative flex w-fit items-center"
+        onMouseEnter={() => {
+          setSelectedIndicator(href);
+        }}
+        custom={index}
+        variants={slide}
+        initial="initial"
+        animate="enter"
+        exit="exit"
+      >
+        <motion.div
+          variants={scale}
+          animate={isActive ? "open" : "closed"}
+          className="absolute left-[-30px] h-[10px] w-[10px] rounded-full bg-white"
+        ></motion.div>
+        <Link href={href} className="font-light text-white no-underline">
+          {title}
+        </Link>
+      </motion.div>
+    </MagneticWrapper>
   );
 }
