e91a9ddd8dc310a4e995c1500e59ca857528775f
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-02-18 20:00:42 +0100

    refactor: extract classes to the target component

--- components/SectionWrapper.tsx
+++ components/SectionWrapper.tsx
@@ -12,7 +12,7 @@ export default function SectionWrapper({
   return (
     <section
       className={cn(
-        "mx-auto bg-white px-xMobile py-yMobile pb-52 pt-0 duration-500 ease-rrSmooth dark:bg-rrDark max-[374px]:pb-52 min-[500px]:px-px500 min-[500px]:pb-yMobile md:px-xTablet md:py-yTablet md:pt-0 lg:py-yDesktop lg:pt-0",
+        "mx-auto bg-white px-xMobile py-yMobile pt-0 duration-500 ease-rrSmooth dark:bg-rrDark min-[500px]:px-px500 min-[500px]:pb-yMobile md:px-xTablet md:py-yTablet md:pt-0 lg:py-yDesktop lg:pt-0",
         className,
       )}
     >
