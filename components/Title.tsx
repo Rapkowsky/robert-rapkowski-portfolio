30585d536888cf5b9e0ece673aea5871f6b1fea2
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-03-09 09:25:54 +0100

    feat: separate title components

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,49 +1,21 @@
 import { cn } from "@/lib/utils";
-import React, { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
+import React from "react";
 
 interface TitleProps {
   text: string;
-  titleClassName?: string;
-  gradientAngle?: string;
-  gradientLightColor?: string;
-  gradientWidth?: string;
-  variant?: "white" | "gray";
+  className?: string;
 }
 
-const Title = ({
-  text,
-  titleClassName,
-  gradientWidth = "150%",
-  variant = "white",
-}: TitleProps) => {
-  const container = useRef(null);
-  const { scrollYProgress } = useScroll({
-    target: container,
-    offset: ["0.3 end", "1000px end"],
-  });
-  const x = useTransform(scrollYProgress, [0, 1], ["-25%", "101%"]);
-
-  const gradientClasses = {
-    white: "bg-[linear-gradient(90deg,_#fff0,_white_25%)] ",
-    gray: "bg-[linear-gradient(90deg,_#fff0,_#f5f5f7_25%)] ",
-  }[variant];
-
+const Title = ({ text, className }: TitleProps) => {
   return (
-    <div className="relative overflow-hidden" ref={container}>
-      <motion.div
-        style={{ x, width: gradientWidth }}
-        className={`pointer-events-none absolute inset-0 z-[1] cursor-none ${gradientClasses} dark:bg-[linear-gradient(90deg,_#fff0,_black_25%)]`}
-      ></motion.div>
-      <h1
-        className={cn(
-          "mb-28 self-start text-center text-[min(16vw,60px)] font-semibold uppercase leading-[1.1] tracking-wider text-rrDark dark:text-white md:mb-40 md:text-8xl xl:text-9xl",
-          titleClassName,
-        )}
-      >
-        {text}
-      </h1>
-    </div>
+    <h1
+      className={cn(
+        "mb-28 self-start text-center text-[min(16vw,60px)] font-semibold uppercase leading-[1.1] tracking-wider text-rrDark dark:text-white md:mb-40 md:text-8xl xl:text-9xl",
+        className,
+      )}
+    >
+      {text}
+    </h1>
   );
 };
 
---
+++ components/TitleSlideLeft.tsx
@@ -0,0 +1,50 @@
+import { cn } from "@/lib/utils";
+import React, { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+interface TitleSlideLeftProps {
+  text: string;
+  titleClassName?: string;
+  gradientAngle?: string;
+  gradientLightColor?: string;
+  gradientWidth?: string;
+  variant?: "white" | "gray";
+}
+
+const TitleSlideLeft = ({
+  text,
+  titleClassName,
+  gradientWidth = "150%",
+  variant = "white",
+}: TitleSlideLeftProps) => {
+  const container = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["0.3 end", "1000px end"],
+  });
+  const x = useTransform(scrollYProgress, [0, 1], ["-25%", "101%"]);
+
+  const gradientClasses = {
+    white: "bg-[linear-gradient(90deg,_#fff0,_white_25%)] ",
+    gray: "bg-[linear-gradient(90deg,_#fff0,_#f5f5f7_25%)] ",
+  }[variant];
+
+  return (
+    <div className="relative overflow-hidden" ref={container}>
+      <motion.div
+        style={{ x, width: gradientWidth }}
+        className={`pointer-events-none absolute inset-0 z-[1] cursor-none ${gradientClasses} dark:bg-[linear-gradient(90deg,_#fff0,_black_25%)]`}
+      ></motion.div>
+      <h1
+        className={cn(
+          "mb-28 self-start text-center text-[min(16vw,60px)] font-semibold uppercase leading-[1.1] tracking-wider text-rrDark dark:text-white md:mb-40 md:text-8xl xl:text-9xl",
+          titleClassName,
+        )}
+      >
+        {text}
+      </h1>
+    </div>
+  );
+};
+
+export default TitleSlideLeft;
