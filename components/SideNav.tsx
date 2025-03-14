07f2bbd7914c54f4c9d910a0fbe47df2e480f9aa
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-03-15 00:17:25 +0100

    style: reduce padding

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -39,7 +39,7 @@ export default function SideNav(isActive: { isActive: boolean }) {
       exit="exit"
       className={`fixed right-0 top-0 z-30 h-screen w-screen bg-rrGray text-white ${isActive && "will-change-transform"} min-[500px]:w-[initial]`}
     >
-      <div className="box-border flex h-full flex-col justify-between px-5 pb-12 pt-8 transition-[padding] duration-800 ease-rrEaseBtnHover min-[500px]:p-[clamp(50px,10vh,140px)_70px] md:px-[clamp(70px,9vw,220px)]">
+      <div className="box-border flex h-full flex-col justify-between px-5 pb-12 pt-8 transition-[padding] duration-800 ease-rrEaseBtnHover min-[500px]:p-[clamp(50px,10vh,140px)_70px] md:px-[clamp(70px,9vw,140px)]">
         <div
           onMouseLeave={() => {
             setSelectedIndicator(pathname);
