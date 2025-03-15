bff37bf4eafbe274bd46977e047ebf9e4c386e71
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-03-15 02:56:48 +0100

    style: improve animation

--- components/BurgerButton.tsx
+++ components/BurgerButton.tsx
@@ -28,10 +28,10 @@ const BurgerButton = forwardRef<HTMLDivElement, BurgerButtonProps>(function (
 
   const burgerContent = (
     <div
-      className={`relative inset-0 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full transition-colors duration-1000 ease-rrEaseBurger lg:h-24 lg:w-24 ${isActive ? "bg-primary" : "bg-rrDark dark:bg-rrGray"}`}
+      className={`relative inset-0 flex h-16 w-16 cursor-pointer items-center justify-center rounded-full transition-colors duration-500 ease-rrEaseBurger lg:h-24 lg:w-24 ${isActive ? "bg-primary" : "bg-rrDark dark:bg-rrGray"}`}
     >
       <div
-        className={`z-20 w-full before:relative before:m-auto before:block before:h-[1px] before:w-[40%] before:bg-white before:transition-transform before:duration-500 before:ease-rrEaseBurgerLines before:content-[''] after:relative after:m-auto after:block after:h-[1px] after:w-[40%] after:bg-white after:transition-transform after:duration-500 after:ease-rrEaseBurgerLines after:content-[''] ${isActive ? "before:top-0 before:-rotate-45 after:top-[-1px] after:rotate-45" : "before:top-[5px] after:top-[-5px]"}`}
+        className={`z-20 w-full before:relative before:m-auto before:block before:h-[1px] before:w-[40%] before:bg-white before:transition-transform before:duration-400 before:ease-rrEaseBurgerLines before:content-[''] after:relative after:m-auto after:block after:h-[1px] after:w-[40%] after:bg-white after:transition-transform after:duration-400 after:ease-rrEaseBurgerLines after:content-[''] ${isActive ? "before:top-0 before:-rotate-45 after:top-[-1px] after:rotate-45" : "before:top-[5px] after:top-[-5px]"}`}
       ></div>
     </div>
   );
@@ -54,7 +54,9 @@ const BurgerButton = forwardRef<HTMLDivElement, BurgerButtonProps>(function (
         )}
       </div>
 
-      <AnimatePresence mode="wait">{isActive && <SideNav isActive={isActive} />}</AnimatePresence>
+      <AnimatePresence mode="wait">
+        {isActive && <SideNav isActive={isActive} />}
+      </AnimatePresence>
     </>
   );
 });
