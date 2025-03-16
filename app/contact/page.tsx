24849f9a4ae96c3703e40a24dfcd18234b4dfab7
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-03-16 20:13:42 +0100

    style: change height

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,7 @@ import ContactForm from "./ContactForm";
 
 const page = () => {
   return (
-    <div className="flex min-h-screen w-full items-center justify-center">
+    <div className="flex min-h-[100svh] w-full items-center justify-center">
       <ContactForm />
     </div>
   );
