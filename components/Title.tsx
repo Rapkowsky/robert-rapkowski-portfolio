e3cd0f8309c69731158236e90a8a3226e4a528ed
Robert Rapkowski <robertrapkowski19@gmail.com>
2025-03-16 20:13:13 +0100

    feat: add variant

--- components/Title.tsx
+++ components/Title.tsx
@@ -1,21 +1,33 @@
 import { cn } from "@/lib/utils";
 import React from "react";
+import { cva } from "class-variance-authority";
 
 interface TitleProps {
   text: string;
   className?: string;
+  variant?: "default" | "gradientColor";
 }
 
-const Title = ({ text, className }: TitleProps) => {
+const titleVariants = cva(
+  "mb-28 self-start text-center text-[min(16vw,60px)] font-semibold uppercase leading-[1.1] tracking-wider md:mb-40 md:text-8xl xl:text-9xl",
+  {
+    variants: {
+      variant: {
+        default: "",
+        gradientColor: "bg-textGradient bg-clip-text text-transparent",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  },
+);
+
+const Title = ({ text, className, variant, ...props }: TitleProps) => {
   return (
-    <h1
-      className={cn(
-        "mb-28 self-start text-center text-[min(16vw,60px)] font-semibold uppercase leading-[1.1] tracking-wider text-rrDark dark:text-white md:mb-40 md:text-8xl xl:text-9xl",
-        className,
-      )}
-    >
+    <h2 {...props} className={cn(titleVariants({ variant }), className)}>
       {text}
-    </h1>
+    </h2>
   );
 };
 
